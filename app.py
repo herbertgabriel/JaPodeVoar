5d1c759d1609c123a39bb2d2b3f1da8343d9fef7
Herbert Gabriel <herbertgacruz@gmail.com>
2024-11-22 21:01:06 -0300

    Corrigir problema de binding da porta

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app
 from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    serve(app, host='0.0.0.0', port=port)
